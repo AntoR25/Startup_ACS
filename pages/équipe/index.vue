f41321e679d65fa4a5b3498912449a1c4bf57a0f
AntoR25 <147382329+AntoR25@users.noreply.github.com>
2024-01-08 21:42:45 +0100

    Update index.vue

--- pages/équipe/index.vue
+++ pages/équipe/index.vue
@@ -20,7 +20,7 @@
       </div>
 
       <div class="group before:hover:scale-95 before:hover:h-72 before:hover:w-80 before:hover:h-44 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-blue-200 to-blue-700 before:absolute before:top-0 w-80 h-72 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden">
-          <img src="https://placekitten.com/400/400" alt="Image aléatoire 2" class="w-28 h-28 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500">
+          <img src="https://placekitten.com/100/100" alt="Image aléatoire 2" class="w-28 h-28 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500">
           <div class="z-10 group-hover:-translate-y-10 transition-all duration-500">
               <span class="text-2xl font-semibold text-black-700">Bob Williams</span>
               <p class="text-black-700">Software Engineer</p>
