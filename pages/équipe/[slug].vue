f888f9f7a438cfeca3207ababaac510c8f99c594
AntoR25 <147382329+AntoR25@users.noreply.github.com>
2024-01-08 12:26:22 +0100

    Update [slug].vue

--- pages/équipe/[slug].vue
+++ pages/équipe/[slug].vue
@@ -1,74 +1,31 @@
-<script setup>
-import { useRoute } from 'vue-router';
-
-const query = gql`
-  query Membre {
-    membre {
-      createdAt
-      id
-      publishedAt
-      name
-      poste
-      updatedAt
-      image {
-        id
-        handle
-        fileName
-        mimeType
-        url
-      }
-      linkedin
-      slug
-    }
-  }
-`;
-
-const route = useRoute();
-const { data } = await useAsyncQuery(query, {
-  slug: route.params.slug,
-});
-
-const membre = ref(data.value.membre);
-</script>
-
 <template>
-  <br><br>
-  <Head v-if="membre">
-    <Title>{{ membre.name }} - Détails du Membre de l'Équipe</Title>
-    <Meta
-      name="description"
-      :content="`Découvrez des détails sur ${membre.name}: ${membre.poste}`"
-    />
-    <Meta
-      property="og:title"
-      :content="`${membre.name} - Détails du Membre de l'Équipe`"
-    />
-    <Meta
-      property="og:description"
-      :content="`Découvrez des détails sur ${membre.name}: ${membre.poste}`"
-    />
-    <!-- Ajoutez des propriétés supplémentaires en fonction de la structure de votre modèle -->
-  </Head>
-
-  <div v-if="membre" class="max-w-full flex flex-col md:flex-row mx-auto p-8 bg-white rounded-lg shadow-md">
-    <img class="w-full md:w-1/2 h-auto rounded mb-4 md:mb-0 md:mr-4" :src="membre.image.url" :alt="membre.name" />
-
-    <div class="md:w-1/2 flex flex-col justify-center">
-      <div class="text-center">
-        <h2 class="text-3xl mb-4">{{ membre.name }}</h2>
-        <p class="text-red-950">{{ membre.poste }}</p>
-        <!-- Ajoutez d'autres informations sur le membre de l'équipe en fonction de votre modèle -->
-        <div class="my-8">
-          <a class="text-sm font-semibold inline-block py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase last:mr-0 mr-4">
-            <Icon name="pajamas:label" size="1.5rem" class="text-gray-100 mr-2" /> Catégorie: {{ membre.categorie }}
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div v-else class="flex items-center justify-center h-screen">
-    <span class="text-lg">Chargement en cours...</span>
-  </div>
   <br><br><br>
+  <section class="flex justify-between items-center">
+      <div class="group before:hover:scale-95 before:hover:h-72 before:hover:w-80 before:hover:h-44 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-blue-200 to-blue-700 before:absolute before:top-0 w-80 h-72 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden">
+          <img src="https://placekitten.com/300/300" alt="Image aléatoire" class="w-28 h-28 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500">
+          <div class="z-10 group-hover:-translate-y-10 transition-all duration-500">
+              <span class="text-2xl font-semibold text-black-700">George Johnson</span>
+              <p class="text-black-700">Support Specialist</p>
+          </div>
+          <a class="bg-blue-700 px-4 py-1 text-slate-50 rounded-md z-10 hover:scale-125 transition-all duration-500 hover:bg-blue-500" href="https://www.linkedin.com/in/georgejohnson" target="_blank">Follow</a>
+      </div>
+
+      <div class="group before:hover:scale-95 before:hover:h-72 before:hover:w-80 before:hover:h-44 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-blue-200 to-blue-700 before:absolute before:top-0 w-80 h-72 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden">
+          <img src="https://placekitten.com/200/200" alt="Image aléatoire 1" class="w-28 h-28 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500">
+          <div class="z-10 group-hover:-translate-y-10 transition-all duration-500">
+              <span class="text-2xl font-semibold text-black-700">Alice Thompson</span>
+              <p class="text-black-700">Graphic Designer</p>
+          </div>
+          <a class="bg-blue-700 px-4 py-1 text-slate-50 rounded-md z-10 hover:scale-125 transition-all duration-500 hover:bg-blue-500" href="https://www.linkedin.com/in/alicethompson" target="_blank">Follow</a>
+      </div>
+
+      <div class="group before:hover:scale-95 before:hover:h-72 before:hover:w-80 before:hover:h-44 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-blue-200 to-blue-700 before:absolute before:top-0 w-80 h-72 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden">
+          <img src="https://placekitten.com/400/400" alt="Image aléatoire 2" class="w-28 h-28 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500">
+          <div class="z-10 group-hover:-translate-y-10 transition-all duration-500">
+              <span class="text-2xl font-semibold text-black-700">Bob Williams</span>
+              <p class="text-black-700">Software Engineer</p>
+          </div>
+          <a class="bg-blue-700 px-4 py-1 text-slate-50 rounded-md z-10 hover:scale-125 transition-all duration-500 hover:bg-blue-500" href="https://www.linkedin.com/in/bobwilliams" target="_blank">Follow</a>
+      </div>
+  </section>
 </template>
